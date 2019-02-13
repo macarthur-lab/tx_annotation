6bbdc1bcb871fe1a0f7cd76f32591f8fe307621c
Besse Cummings <berylc@broadinstitute.org>
2019-02-13 10:28:25 -0500

    trying to put the mean proportion calculation in the right place :/

--- tx_annotation.py
+++ tx_annotation.py
@@ -237,7 +237,7 @@ def get_expression_proportion(tx_table, tissues_to_filter, gene_maximum_kt):
 
     expression_proportion_table = expression_proportion_table.annotate(
         mean_proportion=hl.mean( hl.filter(
-            lambda e : ~hl.is_nan(e),  [expression_proportion_table[tissue_id] for tissue_id in remaining_tissue_columns), filter_missing=True))
+            lambda e : ~hl.is_nan(e),  [expression_proportion_table[tissue_id] for tissue_id in remaining_tissue_columns], filter_missing=True))
 
     expression_proportion_table = expression_proportion_table.drop(
         expression_proportion_table.expression_proportion_dict).key_by(
