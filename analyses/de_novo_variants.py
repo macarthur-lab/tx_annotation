755b51c0586d58f79453f9936049a0a9f52c0d89
Besse Cummings <berylc@broadinstitute.org>
2019-02-18 13:20:42 -0500

    salmon

--- analyses/de_novo_variants.py
+++ analyses/de_novo_variants.py
@@ -1,5 +1,5 @@
 from tx_annotation import *
-
+'''
 rt = hl.import_table("gs://gnomad-berylc/tx-annotation/hail2/DeNovoSignal/data/updated_includeASC_Hamdam/ASD_ddid_Updated.hamdam.ASC.011418.txt")
 rt = rt.annotate(variant=rt.CHROM + ':' + rt.POSITION + ":" + rt.REF + ":" + rt.ALT)
 rt = rt.annotate(** hl.parse_variant(rt.variant))
@@ -8,3 +8,14 @@ mt = hl.MatrixTable.from_rows_table(rt)
 mt = mt.repartition(10)
 annotated_mt = hl.vep(mt, vep_config)
 annotated_mt.write("gs://gnomad-public/papers/2019-tx-annotation/results/de_novo_variants/asd_ddid_de_novos.vepped.021819.mt")
+'''
+
+
+mt, gtex = read_tx_annotation_tables(ddid_asd_de_novos, gtex_v7_tx_summary_mt_path, "mt")
+ddid_asd = tx_annotate_mt(mt, gtex, "proportion",
+                          filter_to_csqs=all_coding_csqs)
+ddid_asd = ddid_asd.filter_rows(~hl.is_missing(ddid_asd.tx_annotation))
+ddid_asd = ddid_asd.annotate_rows(tx_annotation=ddid_asd.tx_annotation.map(fix_loftee_beta_nonlofs))
+ddid_asd = pull_out_worst_from_tx_annotate(ddid_asd)
+ddid_asd.rows().export("gs://gnomad-public/papers/2019-tx-annotation/results/de_novo_variants/"
+                       "asd_ddid_de_novos.tx_annotated.021819.tsv.bgz")
--- analyses/rsem_vs_salmon.py
+++ analyses/rsem_vs_salmon.py
@@ -10,7 +10,7 @@ gene_id
 '''
 
 # Make median tissue expression files to be used for analysis
-'''
+
 salmon_brain_cortex_path = "gs://gnomad-berylc/tx-annotation/hail2/salmon_rsem/salmon.GTEx.v7.brain.cortex.021419.tsv"
 salmon_brain_cortex_out_path = "gs://gnomad-berylc/tx-annotation/hail2/salmon_rsem/salmon.GTEx.v7.brain.cortex.tx_medians.021419.mt"
 get_gtex_summary(salmon_brain_cortex_path, salmon_brain_cortex_out_path, get_medians=True, make_per_tissue_file=None)
@@ -18,11 +18,20 @@ get_gtex_summary(salmon_brain_cortex_path, salmon_brain_cortex_out_path, get_med
 rsem_brain_cortex_path = "gs://gnomad-berylc/tx-annotation/hail2/salmon_rsem/rsem.GTEx.v7.brain.cortex.021419.tsv"
 rsem_brain_cortex_out_path = "gs://gnomad-berylc/tx-annotation/hail2/salmon_rsem/rsem.GTEx.v7.brain.cortex.tx_medians.021419.mt"
 get_gtex_summary(rsem_brain_cortex_path, rsem_brain_cortex_out_path, get_medians=True, make_per_tissue_file=None)
-'''
-# Run TCF4 on both
 
+# Get baselevel TCF4 pext values on both
+salmon_brain_cortex_out_path = "gs://gnomad-berylc/tx-annotation/hail2/salmon_rsem/salmon.GTEx.v7.brain.cortex.tx_medians.021519.mt"
+mt, salmon = read_tx_annotation_tables(context_ht_path, salmon_brain_cortex_out_path, "ht")
+salmon_baselevel_prop= get_baselevel_expression_for_genes(mt, salmon, gene_list = {'TCF4'}, get_proportions=True)
+salmon_baselevel_prop.export("gs://gnomad-berylc/tx-annotation/hail2/salmon_rsem/TCF4/salmon.TCF4.baselevel.pext.021519.tsv.bgz")
 
-# Run MAPs based on both
+rsem_brain_cortex_path = "gs://gnomad-berylc/tx-annotation/hail2/salmon_rsem/rsem.GTEx.v7.brain.cortex.tx_medians.021519.mt"
+mt, rsem = read_tx_annotation_tables(context_ht_path, rsem_brain_cortex_path, "ht")
+rsem_baselevel_prop= get_baselevel_expression_for_genes(mt, rsem, gene_list = {'TCF4'}, get_proportions=True)
+rsem_baselevel_prop.export("gs://gnomad-berylc/tx-annotation/hail2/salmon_rsem/TCF4/rsem.TCF4.baselevel.pext.021519.tsv.bgz")
+
+#  To run MAPs on both, first annotate all of gnomAD exomes with both files, then follow the maps_submit_per_class.py script
+# replacing the HT on line 32 with the files you create here
 ''' First had to annotate gnomAD with each e.g.: 
 latest_gnomad_120518 = "gs://gnomad-public/release/2.1.1/ht/exomes/gnomad.exomes.r2.1.1.sites.ht"
 rsem_brain_cortex_out_path = "gs://gnomad-berylc/tx-annotation/hail2/salmon_rsem/rsem.GTEx.v7.brain.cortex.tx_medians.021519.mt"
@@ -33,12 +42,10 @@ mt_annotated = tx_annotate_mt(mt, gtex,"proportion", tissues_to_filter = None,
 mt_annotated.write("gs://gnomad-berylc/tx-annotation/hail2/salmon_rsem/rsem.brain_cortex.gnomad.exomes.r2.1.1.sites.tx_annotated.021519.ht")
 '''
 
-# Run HI variant filtering based on both
-
-
 # Gene list annotation
-from tx_annotation import *
-'''
+
+# Salmon
+# gnomAD
 salmon_brain_cortex_out_path = "gs://gnomad-berylc/tx-annotation/hail2/salmon_rsem/salmon.GTEx.v7.brain.cortex.tx_medians.021519.mt"
 clinvar_mt, salmon = read_tx_annotation_tables(clinvar_ht_path, salmon_brain_cortex_out_path, "ht")
 mt, salmon = read_tx_annotation_tables(gnomad_release_mt_path, salmon_brain_cortex_out_path, "ht")
@@ -56,6 +63,7 @@ mt_gnomad_hi = mt_gnomad_hi.filter_rows(~hl.is_missing(mt_gnomad_hi.tx_annotatio
 mt_gnomad_hi = pull_out_worst_from_tx_annotate(mt_gnomad_hi)
 mt_gnomad_hi.rows().export("%sHI_genes.salmon.brain_cortex.gnomad.exomes.r2.1.tx_annotated.021519.tsv.bgz" %out_dir)
 
+# ClinVar
 mt_clinvar_hi = tx_annotate_mt(clinvar_mt, salmon,"proportion",tissues_to_filter = None,
                                filter_to_csqs=lof_csqs, filter_to_genes=hi_genes,
                                gene_column_in_mt="gene_id")
@@ -64,9 +72,10 @@ mt_clinvar_hi = pull_out_worst_from_tx_annotate(mt_clinvar_hi)
 mt_clinvar_hi = mt_clinvar_hi.annotate_rows(**mt_clinvar_hi.info)
 mt_clinvar_hi = mt_clinvar_hi.drop("vep", "tx_annotation","info")
 mt_clinvar_hi.rows().export("%sHI_genes.salmon.brain_cortex.clinvar.alleles.single.b37.tx_annotated.021519.tsv.bgz" %out_dir)
-'''
 
-'''
+
+# RSEM
+# gnomAD
 rsem_brain_cortex_path = "gs://gnomad-berylc/tx-annotation/hail2/salmon_rsem/rsem.GTEx.v7.brain.cortex.tx_medians.021519.mt"
 clinvar_mt, rsem = read_tx_annotation_tables(clinvar_ht_path, rsem_brain_cortex_path, "ht")
 mt, rsem = read_tx_annotation_tables(gnomad_release_mt_path, rsem_brain_cortex_path, "ht")
@@ -93,8 +102,7 @@ mt_clinvar_hi = mt_clinvar_hi.annotate_rows(**mt_clinvar_hi.info)
 mt_clinvar_hi = mt_clinvar_hi.drop("vep", "tx_annotation","info")
 mt_clinvar_hi.rows().export("%sHI_genes.rsem.brain_cortex.clinvar.alleles.single.b37.tx_annotated.021519.tsv.bgz" %out_dir)
 
-'''
-
+# Don't forget gnomAD genomes!
 
 salmon_brain_cortex_out_path = "gs://gnomad-berylc/tx-annotation/hail2/salmon_rsem/salmon.GTEx.v7.brain.cortex.tx_medians.021519.mt"
 clinvar_mt, salmon = read_tx_annotation_tables(clinvar_ht_path, salmon_brain_cortex_out_path, "ht")
