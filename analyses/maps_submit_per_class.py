044ed3d808dbbb12ca8aa73a4d6cbb28ff42892c
Besse Cummings <berylc@broadinstitute.org>
2019-06-20 15:44:19 -0400

    context ht path disambiguated

--- analyses/maps_submit_per_class.py
+++ analyses/maps_submit_per_class.py
@@ -25,7 +25,6 @@ def load_tx_expression_data(tx_ht):
 
     return tx_ht.annotate(tx_annotation=tx_ht.tx_annotation.map(process_expression_data))
 
-context_ht_path = 'gs://gnomad-resources/context/hail-0.2/Homo_sapiens_assembly19.fasta.snps_only.vep_20181129.ht'
 context_ht = hl.read_table(context_ht_path)
 
 # Import and process gnomad 2.1.1 transcript annotation
