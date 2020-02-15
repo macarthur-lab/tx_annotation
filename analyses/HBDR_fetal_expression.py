126f446de61baabc940dd7849b6bab9c9fe01278
Besse Cummings <berylc@broadinstitute.org>
2020-02-15 08:29:18 -0800

    full pipeline

--- analyses/HBDR_fetal_expression.py
+++ analyses/HBDR_fetal_expression.py
@@ -1,54 +1,33 @@
-'''Code snippet to annotate all possible SNVs pext values. Ran on 100 non-preemptible nodes'''
-
 from tx_annotation import *
-from datetime import datetime
-'''
 
-print(gtex_v7_tx_summary_ht_path)
-print(gtex_v7_gene_maximums_ht_path)
+rsem_tpms = "gs://gnomad-public/papers/2019-tx-annotation/data/HBDR_fetal_RNAseq/HBDR.RSEM.tissue_names.tpm.tsv.gz"
 
-start = datetime.now()
-print("Starting on",start)
+get_gtex_summary(rsem_tpms, "gs://gnomad-public/papers/2019-tx-annotation/data/HBDR_fetal_RNAseq/HBDR.tissue_names.medians.021520.mt")
 
-mt, gtex = read_tx_annotation_tables(context_ht_path, gtex_v7_tx_summary_ht_path, 'ht')
-start_annotation = datetime.now()
-
-print("Starting tx annotation on", start_annotation)
-
-context_hg19_annotated = tx_annotate_mt(mt, gtex, "proportion",
-                                        tissues_to_filter=None,
-                                        gene_maximums_ht_path=gtex_v7_gene_maximums_ht_path,
-                                        filter_to_csqs=all_coding_csqs)
-
-finished_annotation = datetime.now()
-print("Finished annotation ",finished_annotation)
+get_gene_expression("gs://gnomad-public/papers/2019-tx-annotation/data/HBDR_fetal_RNAseq/HBDR.tissue_names.medians.021520.mt",
+                    "gs://gnomad-public/papers/2019-tx-annotation/data/HBDR.gene_expression_per_gene_per_tissue.021520.ht")
 
 
-context_hg19_annotated.write("gs://gnomad-public/papers/2019-tx-annotation/pre_computed/all.possible.snvs.tx_annotated.021520.ht",overwrite=True)
+# At this point updated hbdr_fetal_tissue_summary_ht_path, hbdr_fetal_tissue_gene_maximums_ht_path in tx_annotation_resources.py
+print(hbdr_tx_summary_mt_path)
+print(hbdr_gene_maximums_ht_path)
 
-# get maximum pext per gene to identify genes to filter from future analyses
-identify_maximum_pext_per_gene("gs://gnomad-public/papers/2019-tx-annotation/pre_computed/all.possible.snvs.tx_annotated.021520.ht",
-                               "gs://gnomad-public/papers/2019-tx-annotation/data/GRCH37_hg19/all.genes.max.pext.GTEx.v7.021520.tsv.bgz")
+context_hbdr_annotated = "gs://gnomad-public/papers/2019-tx-annotation/data/HBDR_fetal_RNAseq/all.possible.snvs.tx_annotated.HBDR.021520.ht"
+context_hbdr_max_per_gene = "gs://gnomad-public/papers/2019-tx-annotation/data/HBDR_fetal_RNAseq/all.genes.max.pext.HBDR.021520.tsv.bgz"
 
-stop = datetime.now()
-print("Finished writing",stop)
-print("total time", stop - start)
+mt, hbdr = read_tx_annotation_tables(context_ht_path, hbdr_tx_summary_mt_path, 'ht')
 
-# Now post-processing to enable users to extract tx annotation without hail
-start_post = datetime.now()
+print("Starting tx annotation on")
 
-print("Starting post-processing for those who don't want to use Hail",start_post)
+mt_annotated = tx_annotate_mt(mt, hbdr, "proportion",
+                              tissues_to_filter=None,
+                              gene_maximums_ht_path=hbdr_gene_maximums_ht_path,
+                              filter_to_csqs=all_coding_csqs)
 
-context_hg19_annotated = context_hg19_annotated.filter_rows(~hl.is_missing(context_hg19_annotated.tx_annotation))
-context_hg19_annotated = context_hg19_annotated.key_rows_by()
-context_hg19_annotated = context_hg19_annotated.select_rows("locus", "alleles", "tx_annotation")
-context_hg19_annotated = context_hg19_annotated.annotate_rows(chrom=context_hg19_annotated.locus.contig,
-                                                              pos=context_hg19_annotated.locus.position,
-                                                              ref=context_hg19_annotated.alleles[0],
-                                                              alt=context_hg19_annotated.alleles[1])
+print("Finished annotation ")
 
-context_hg19_annotated = context_hg19_annotated.select_rows("chrom", "pos", "ref", "alt", "tx_annotation")
-context_hg19_annotated.count_rows()
-context_hg19_annotated.rows().export("gs://gnomad-public/papers/2019-tx-annotation/pre_computed/all.possible.snvs.tx_annotated.GTEx.v7.021520.tsv.bgz")
-'''
+mt_annotated.write(context_hbdr_annotated, overwrite=True)
 
+print("Wrote RSEM Brain Cortex HT, now going to identify maximums")
+
+identify_maximum_pext_per_gene(context_hbdr_annotated, context_hbdr_max_per_gene)
